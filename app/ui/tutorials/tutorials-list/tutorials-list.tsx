77dadf84f1fc6be3b868e892f7ca890d67290858
Vi Nguyen <vi@justvi.com>
2024-09-11 20:37:32 -0700

    fix TutorialsList

--- app/ui/tutorials/tutorials-list/tutorials-list.tsx
+++ app/ui/tutorials/tutorials-list/tutorials-list.tsx
@@ -11,33 +11,36 @@ export default async function TutorialsList() {
   }
 
   return (
-    <ul className="grid gap-28 grid-cols-3">
-      {tutorials.map((tutorial) => (
-        <li key={tutorial.id}>
-          <Link href={`/tutorials/${tutorial.slug}`}>
-            <div className="flex flex-col space-y-8 items-center text-[#131309] rounded-md text-lg">
-              <div>
-                {images.blobs
-                  .filter((image) => image.pathname.startsWith(`homepage_${tutorial.slug}`))
-                  .map((image) => (
-                    <Image
-                      className="rounded-lg w-96 h-96"
-                      priority
-                      key={image.pathname}
-                      // src={tutorial.image_url}
-                      src={image.url}
-                      alt={tutorial.title}
-                      width={1064}
-                      height={1064}
-                    />
-                  ))}
+    <div>
+      <ul className="grid gap-28 grid-cols-3">
+        {tutorials.map((tutorial) => (
+          <li key={tutorial.id}>
+            <Link href={`/tutorials/${tutorial.slug}`}>
+              <div className="flex flex-col space-y-8 items-center text-[#131309] rounded-md text-lg">
+                <div>
+                  {images.blobs
+                    .filter((image) =>
+                      image.pathname.startsWith(`homepage_${tutorial.slug}`)
+                    )
+                    .map((image) => (
+                      <Image
+                        className="rounded-lg w-96 h-96"
+                        priority
+                        key={image.pathname}
+                        // src={tutorial.image_url}
+                        src={image.url}
+                        alt={tutorial.title}
+                        width={1064}
+                        height={1064}
+                      />
+                    ))}
+                </div>
+                <div>{tutorial.title}</div>
               </div>
-              <div>{tutorial.title}</div>
-            </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
-
