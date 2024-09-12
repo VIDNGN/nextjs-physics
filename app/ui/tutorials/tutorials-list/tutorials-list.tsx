4de8ba409995bbd650191a204c1e3835efc5459b
vidngn <vi@justvi>
2024-09-12 00:08:03 -0700

    change TutorialsList to simple div with just a heading for testing Vercel Deployment.

--- app/ui/tutorials/tutorials-list/tutorials-list.tsx
+++ app/ui/tutorials/tutorials-list/tutorials-list.tsx
@@ -15,35 +15,7 @@ export default async function TutorialsList() {
 
   return (
     <div>
-      <ul className="grid gap-28 grid-cols-3">
-        {tutorials.map((tutorial) => (
-          <li key={tutorial.id}>
-            <Link href={`/tutorials/${tutorial.slug}`}>
-              <div className="flex flex-col space-y-8 items-center text-[#131309] rounded-md text-lg">
-                <div>
-                  {images.blobs
-                    .filter((image) =>
-                      image.pathname.startsWith(`homepage_${tutorial.slug}`)
-                    )
-                    .map((image) => (
-                      <Image
-                        className="rounded-lg w-96 h-96"
-                        priority
-                        key={image.pathname}
-                        // src={tutorial.image_url}
-                        src={image.url}
-                        alt={tutorial.title}
-                        width={1064}
-                        height={1064}
-                      />
-                    ))}
-                </div>
-                <div>{tutorial.title}</div>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1>Tutorials List</h1>
     </div>
   );
 }
