da860d4020b8e745cd77defdc983bd9c9ffea63b
vidngn <vi@justvi>
2024-09-12 00:26:21 -0700

    put back everything

--- app/ui/tutorials/tutorials-list/tutorials-list.tsx
+++ app/ui/tutorials/tutorials-list/tutorials-list.tsx
@@ -2,20 +2,48 @@ import { fetchTutorials, fetchAllImages } from "@/app/lib/data";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function TutorialsList() {
-  //const tutorials = await fetchTutorials();
-  //const images = await fetchAllImages();
+export default async function TutorialsList() {
+  const tutorials = await fetchTutorials();
+  const images = await fetchAllImages();
 
   //console.log('tutorials: ', tutorials);
   //console.log('images: ', images);
   
-  // if (!tutorials || !images ) {
-  //   return null;
-  // }
+  if (!tutorials || !images ) {
+    return null;
+  }
 
   return (
     <div>
-      <h1>Tutorials List</h1>
+      <ul className="grid gap-28 grid-cols-3">
+        {tutorials.map((tutorial) => (
+          <li key={tutorial.id}>
+            <Link href={`/tutorials/${tutorial.slug}`}>
+              <div className="flex flex-col space-y-8 items-center text-[#131309] rounded-md text-lg">
+                <div>
+                  {images.blobs
+                    .filter((image) =>
+                      image.pathname.startsWith(`homepage_${tutorial.slug}`)
+                    )
+                    .map((image) => (
+                      <Image
+                        className="rounded-lg w-96 h-96"
+                        priority
+                        key={image.pathname}
+                        // src={tutorial.image_url}
+                        src={image.url}
+                        alt={tutorial.title}
+                        width={1064}
+                        height={1064}
+                      />
+                    ))}
+                </div>
+                <div>{tutorial.title}</div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
