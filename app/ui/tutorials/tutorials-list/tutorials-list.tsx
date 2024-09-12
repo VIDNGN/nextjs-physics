50850ab0bd9e44cfa7bd42687d90b65fadcac807
vidngn <vi@justvi>
2024-09-12 00:11:04 -0700

    removed async, fetching functions

--- app/ui/tutorials/tutorials-list/tutorials-list.tsx
+++ app/ui/tutorials/tutorials-list/tutorials-list.tsx
@@ -2,16 +2,16 @@ import { fetchTutorials, fetchAllImages } from "@/app/lib/data";
 import Link from "next/link";
 import Image from "next/image";
 
-export default async function TutorialsList() {
-  const tutorials = await fetchTutorials();
-  const images = await fetchAllImages();
+export default function TutorialsList() {
+  //const tutorials = await fetchTutorials();
+  //const images = await fetchAllImages();
 
   //console.log('tutorials: ', tutorials);
   //console.log('images: ', images);
   
-  if (!tutorials || !images ) {
-    return null;
-  }
+  // if (!tutorials || !images ) {
+  //   return null;
+  // }
 
   return (
     <div>
