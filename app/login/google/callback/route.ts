45e6bf8bfa2049ff799faa77a81ff4ad62b1e67d
Vi Nguyen <vi@justvi.com>
2024-09-21 13:44:37 -0700

    update checking if .env variables are null or undefined.

--- app/login/google/callback/route.ts
+++ app/login/google/callback/route.ts
@@ -2,93 +2,117 @@ import { google, lucia } from "@/app/lib/session";
 import { cookies } from "next/headers";
 import { OAuth2RequestError } from "arctic";
 //import { generateIdFromEntropySize } from "lucia";
-import { uuid } from 'uuidv4';
+import { uuid } from "uuidv4";
 import { GoogleUser } from "@/app/lib/definitions";
-import { User } from '@/app/lib/definitions';
-import { sql } from '@vercel/postgres';
+import { User } from "@/app/lib/definitions";
+import { sql } from "@vercel/postgres";
 
-async function getUser (google_id: string): Promise<User | undefined> {
-    try {
-        const user = await sql<User>`SELECT users.id, users.name, users.email, users.google_id, users.picture FROM users WHERE google_id = ${google_id};`
-        return user.rows[0];
-    } catch (error){
-        console.error("Failed to fetch user with google_id:", error);
-        throw new Error("Failed to fetch user with google_id.");
-    }
+async function getUser(google_id: string): Promise<User | undefined> {
+  try {
+    const user =
+      await sql<User>`SELECT users.id, users.name, users.email, users.google_id, users.picture FROM users WHERE google_id = ${google_id};`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user with google_id:", error);
+    throw new Error("Failed to fetch user with google_id.");
+  }
 }
 
 export async function GET(request: Request): Promise<Response> {
-	const url = new URL(request.url);
-	const code = url.searchParams.get("code");
-	const state = url.searchParams.get("state");
-    const codeVerifier = process.env.AUTH_SECRET;
-	const storedState = cookies().get("google_oauth_state")?.value ?? null;
-	if (!code || !state || !storedState || state !== storedState) {
-		return new Response(null, {
-			status: 400
-		});
-	}
+  if (!process.env.AUTH_SECRET) {
+    throw new Error("Auth Secret are required!");
+  }
 
-    try {
-        const tokens = await google.validateAuthorizationCode(code, codeVerifier);
-        const googleUserResponse = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
-            headers:{
-                Authorization: `Bearer ${tokens.accessToken}`
-            }
-        });
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  const state = url.searchParams.get("state");
 
-        const googleUser: GoogleUser = await googleUserResponse.json();
+  const codeVerifier = process.env.AUTH_SECRET;
+  const storedState = cookies().get("google_oauth_state")?.value ?? null;
+  if (!code || !state || !storedState || state !== storedState) {
+    return new Response(null, {
+      status: 400,
+    });
+  }
 
-        if (!googleUser) {
-            return new Response(null, {
-                status: 500
-            });
-        }
+  try {
+    const tokens = await google.validateAuthorizationCode(code, codeVerifier);
+    const googleUserResponse = await fetch(
+      "https://openidconnect.googleapis.com/v1/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${tokens.accessToken}`,
+        },
+      }
+    );
 
-        const existingUser = await getUser(googleUser.sub); //googleUser sub looks like id. In db, sub is google_id.
-        
-        if (existingUser) {
-            const {id, name, email, google_id, picture } = existingUser;
-            const session = await lucia.createSession(existingUser.id, {email, google_id, name, picture}); //lucia will use {email, google_id, name, picture} these names and look for db column names in user_sessions table.
-			const sessionCookie = lucia.createSessionCookie(session.id);
-			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			return new Response(null, {
-				status: 302,
-				headers: {
-					Location: "/chat"
-				}
-			});
-        }
+    const googleUser: GoogleUser = await googleUserResponse.json();
 
-        //const userId = generateIdFromEntropySize(10); // 16 characters long
-        const userId = uuid();
-
-        const { sub, name, picture, email } = googleUser; 
-        
-        const google_id = sub; //since user_sessions db table has a column named google_id which is for sub
-
-        await sql`INSERT INTO users (id, name, email, password, google_id, picture) VALUES (${userId}, ${googleUser.name}, ${googleUser.email}, 'google_password', ${googleUser.sub}, ${googleUser.picture});`
-        
-        const session = await lucia.createSession(userId, {email, google_id, name, picture});
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-		return new Response(null, {
-			status: 302,
-			headers: {
-				Location: "/chat"
-			}
-		});
-
-    } catch (error) {
-        if (error instanceof OAuth2RequestError ){
-            return new Response(null, {
-                status:400
-            });
-        }
-        return new Response(null, {
-            status: 500
-        });
+    if (!googleUser) {
+      return new Response(null, {
+        status: 500,
+      });
     }
 
+    const existingUser = await getUser(googleUser.sub); //googleUser sub looks like id. In db, sub is google_id.
 
+    if (existingUser) {
+      const { id, name, email, google_id, picture } = existingUser;
+      const session = await lucia.createSession(existingUser.id, {
+        email,
+        google_id,
+        name,
+        picture,
+      }); //lucia will use {email, google_id, name, picture} these names and look for db column names in user_sessions table.
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+      return new Response(null, {
+        status: 302,
+        headers: {
+          Location: "/chat",
+        },
+      });
+    }
+
+    //const userId = generateIdFromEntropySize(10); // 16 characters long
+    const userId = uuid();
+
+    const { sub, name, picture, email } = googleUser;
+
+    const google_id = sub; //since user_sessions db table has a column named google_id which is for sub
+
+    await sql`INSERT INTO users (id, name, email, password, google_id, picture) VALUES (${userId}, ${googleUser.name}, ${googleUser.email}, 'google_password', ${googleUser.sub}, ${googleUser.picture});`;
+
+    const session = await lucia.createSession(userId, {
+      email,
+      google_id,
+      name,
+      picture,
+    });
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: "/chat",
+      },
+    });
+  } catch (error) {
+    if (error instanceof OAuth2RequestError) {
+      return new Response(null, {
+        status: 400,
+      });
+    }
+    return new Response(null, {
+      status: 500,
+    });
+  }
 }
--- app/login/google/route.ts
+++ app/login/google/route.ts
@@ -5,6 +5,12 @@ import { cookies } from "next/headers";
 
 
 export async function GET(): Promise<Response> {
+
+    
+if (!process.env.AUTH_SECRET) {
+    throw new Error("Auth Secret are required!");
+  }
+
     const codeVerifier = process.env.AUTH_SECRET;
 	const state = generateState();
 	const url : URL = await google.createAuthorizationURL(state, codeVerifier, {scopes: ["email", "profile"]});
