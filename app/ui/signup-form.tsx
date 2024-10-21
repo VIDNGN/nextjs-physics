15c4a10801642a42691522a09b4625d1692582cd
Vi Nguyen <vi@justvi.com>
2024-10-20 18:17:49 -0700

    remove signup form name padding

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -50,7 +50,7 @@ export default function SignUpForm() {
             <div>
               <div className="flex justify-right">
                 <label
-                  className="mb-3 mt-5 px-3 block text-xs font-medium text-gray-900"
+                  className="mb-3 mt-5 block text-xs font-medium text-gray-900"
                   htmlFor="name"
                 >
                   Name
