ac60ebb4a4ebf647ebafd721d654e792ef7fe097
Vi Nguyen <vi@justvi.com>
2024-10-21 19:11:33 -0700

    Updae button class to have onClick event as a parameter.

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -7,13 +7,25 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+export function Button({ children, className, onClick, ...rest }: ButtonProps) {
+
+  const handleClick = () => {
+
+    //send the GTM event
+    sendGTMEvent({event: 'buttonClicked', value: 'xyz'})
+
+    //call the passed onClick function (like openModal), if exists
+    if (onClick) {
+      onClick();
+    } 
+  }
+
+  
 
  // const status = useFormStatus();
   return (
     <button
-      {...rest}
-      onClick={() => sendGTMEvent({event: 'buttonClicked', value: 'xyz'})}
+      {...rest} onClick={handleClick}
       className={clsx(
         'flex h-10 items-center rounded-lg bg-[#27374D] px-4 text-sm font-medium text-white transition-colors hover:bg-[#526D82] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-[#526D82] active:bg-[#27374D] aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
         className,
